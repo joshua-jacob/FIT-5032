381122d8f1f6cacbbfc28f7b4ee9623b0ef27ec2
Joshua Jacob <joshua.jacob@spookfish.in>
2024-08-01 09:44:06 +1000

    week 1 and week 2

---
+++ fit5032-week1/src/components/Joshua.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <h1 class="green">Hello from Joshua</h1>
+        <h3>This is my github <a class="green"href='https://github.com/joshua-jacob'>https://github.com/joshua-jacob</a></h3>
+    </div>
+</template>
